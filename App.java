4e660ca33ff893e880e54a84478a34c7360b5f64
Bruno Lippert <brunohlippert@gmail.com>
2019-06-18 17:05:59 -0300

    os guri

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
+import java.math.BigInteger;
+
 public class App{
     public static void main(String[] agrs){
-        Mips mips = new Mips();
+       Mips mips = new Mips();
+    //    System.out.println(new BigInteger("10000000000000000000000000000000", 2).intValue());
+    //     System.out.println(Integer.toString(-44, 2));
+
     }
 }
--- ULA.java
+++ ULA.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class ULA {
     private static ULA instance;
 
@@ -5,7 +7,7 @@ public class ULA {
 
     }
 
-    public static ULA getInstane() {
+    public static ULA getInstance() {
         if (instance == null)
             instance = new ULA();
         return instance;
@@ -13,33 +15,64 @@ public class ULA {
 
     // entB pode ser shamt
     public String calcula(String entA, String entB, String func) {
-        int valueA = Integer.parseInt(entA, 2);
-        int valueB = Integer.parseInt(entB, 2);
+        int valueA = new BigInteger(entA, 2).intValue();
+        int valueB = new BigInteger(entB, 2).intValue();
         int result;
-        if (Controle.getInstane().ULAOp.equals("00")) {// ADDU/ADDIU//lw/sw
+        if (Controle.getInstance().ULAOp.equals("000")) {// ######## ADDU/ADDIU//lw/sw #######
             result = valueA + valueB;
-        } else if (Controle.getInstane().ULAOp.equals("01")) {// SUB/beq
+        } else if (Controle.getInstance().ULAOp.equals("001")) {// ######## SUB/beq ######
             result = valueA - valueB;
-        } else if (Controle.getInstane().ULAOp.equals("10")) {// ORI
+        } else if (Controle.getInstance().ULAOp.equals("010")) {// ######### ORI ########
             String strResult = "";
             String[] A_array = entA.split("");
             String[] B_array = entB.split("");
             for (int i = 0; i < 32; i++) {
-                if(A_array[i].equals("1") || B_array[i].equals("1")){
+                if (A_array[i].equals("1") || B_array[i].equals("1")) {
                     strResult += "1";
-                }else{
+                } else {
                     strResult += "0";
                 }
             }
             return strResult;
-        } else if (Controle.getInstane().ULAOp.equals("11")) {// lui
-            String res = entB.sub(15);
-            return res+"0000000000000000";
-        } else {// tipo R: slt, and, sll, srl.
-            if(func.equals("100100")){//and
-                
+        } else if (Controle.getInstance().ULAOp.equals("011")) {// ############# lui ##############
+            String res = entB.substring(15);
+            return res + "0000000000000000";
+        } else {// ############## tipo R: slt, and, sll, srl. ################ ULAOp 001
+            if (func.equals("100100")) { // ############ and #############
+                String strResult = "";
+                String[] A_array = entA.split("");
+                String[] B_array = entB.split("");
+                for (int i = 0; i < 32; i++) {
+                    if (A_array[i].equals("1") && B_array[i].equals("1")) {
+                        strResult += "1";
+                    } else {
+                        strResult += "0";
+                    }
+                }
+                return strResult;
+            } else if (func.equals("101010")) {// ############## slt ##############
+                if (valueA < valueB) {
+                    return "00000000000000000000000000000001";
+                } else {
+                    return "00000000000000000000000000000000";
+                }
+            } else if (func.equals("000000")) {//########### sll ###############
+                result = valueA;
+                for (int i = 0; i < valueB; i++) {
+                    result *= 2;
+                }
+            } else if(func.equals("000010")){//########### slr ###############
+                result = valueA;
+                for (int i = 0; i < valueB; i++) {
+                    result /= 2;
+                }
             }
         }
-        return "";
+        String res = Integer.toString(result, 2);
+        res = res.replace("-", "");
+        for (int i = 0; i < 32 - res.length();i++) {
+            res = "0"+res;
+        }
+        return res;
     }
 }
