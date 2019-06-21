e319c96bbfb344699ba5cdd92db946925dee5360
16105090 <16105090@facin.portoalegre.pucrsnet.br>
2019-06-21 16:46:23 -0300

    Sinais de controle das etapas setados

--- Controle.java
+++ Controle.java
@@ -42,8 +42,26 @@ public class Controle {
         proximoEstado = estados.BUSCA;
     }
 
-    public void buscaInstrucao(String instrucao){
+    public boolean buscaInstrucao(String instrucao){
         instrucaoAtual = instrucoes.valueOf(instrucao);
+        PCEscCond = "0";
+        PCEsc = "1"; //Em 1
+        louD = "0"; //Em 0
+        LerMem = "1"; //Em 1
+        EscMem = "0";
+        MemParaReg = "0";
+        IREsc = "1"; //Em 1
+        FontePC = "00"; //Em 00
+        ULAOp = "000"; //Em 00
+        ULAFonteB = "01"; //Em 01
+        ULAFonteA = "0"; //Em 0
+        EscReg = "0";
+        RegDst = "0";
+        return false;
+    }
+
+
+    public boolean entraDecode(){
         PCEscCond = "0";
         PCEsc = "1";
         louD = "0";
@@ -52,48 +70,185 @@ public class Controle {
         MemParaReg = "0";
         IREsc = "1";
         FontePC = "00";
-        ULAOp = "000";
-        ULAFonteB = "01";
-        ULAFonteA = "0";
+        ULAOp = "000"; //Em 00
+        ULAFonteB = "11"; //Em 11
+        ULAFonteA = "0"; //Em 0
         EscReg = "0";
         RegDst = "0";
-    }
-
-    public void entraDecode(){
-        PCEscCond = "0";
-        PCEsc = "0";
-        louD = "0";
-        LerMem = "0";
-        EscMem = "0";
-        MemParaReg = "0";
-        IREsc = "0";
-        FontePC = "00";
-        ULAOp = "000";
-        ULAFonteB = "11";
-        ULAFonteA = "0";
-        EscReg = "0";
-        RegDst = "0";
+        proximoEstado = estados.EXEC;
+        return false;
     }
 
     public boolean entraExec(){
-        //proximoEstado = estados.EXEC;
-        
-        return false;
+        switch (this.instrucaoAtual){
+            case(instrucoes.Jump):
+                PCEscCond = "0";
+                PCEsc = "1";//Em 1
+                louD = "0";
+                LerMem = "1";
+                EscMem = "0";
+                MemParaReg = "0";
+                IREsc = "1";
+                FontePC = "10";//Em 10
+                ULAOp = "000";
+                ULAFonteB = "01";
+                ULAFonteA = "0";
+                EscReg = "0";
+                RegDst = "0";
+                proximoEstado = estados.BUSCA;
+                return true;
+                break;
+            case(instrucoes.Beq):
+                PCEscCond = "1"; //Em 1
+                PCEsc = "0"; //Em 0
+                louD = "0";
+                LerMem = "1";
+                EscMem = "0";
+                MemParaReg = "0";
+                IREsc = "1";
+                FontePC = "01"; //Em 01
+                ULAOp = "001"; //Em 01
+                ULAFonteB = "00"; //Em 00
+                ULAFonteA = "1"; //Em 1
+                EscReg = "0";
+                RegDst = "0";
+                proximoEstado = estados.BUSCA;
+                return true;
+                break;
+            case(instrucoes.Tipo_R):
+                PCEscCond = "0";
+                PCEsc = "1";
+                louD = "0";
+                LerMem = "1";
+                EscMem = "0";
+                MemParaReg = "0";
+                IREsc = "1";
+                FontePC = "00";
+                ULAOp = "010"; //Em 10
+                ULAFonteB = "00"; //Em 00
+                ULAFonteA = "1"; //Em 1
+                EscReg = "0";
+                RegDst = "0";
+                proximoEstado = estados.MEMORIA;
+                return false;
+                break;
+            case(instrucoes.Lw):
+            case(instrucoes.Sw):
+                PCEscCond = "0";
+                PCEsc = "1";
+                louD = "0";
+                LerMem = "1";
+                EscMem = "0";
+                MemParaReg = "0";
+                IREsc = "1";
+                FontePC = "00";
+                ULAOp = "000"; //Em 00
+                ULAFonteB = "10"; //Em 10
+                ULAFonteA = "1"; //Em 1
+                EscReg = "0";
+                RegDst = "0";
+                proximoEstado  = estados.MEMORIA;
+                return false;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public boolean entraMemoria(){
+        switch(instrucaoAtual){
+            //Poderia ser o deafult
+            case instrucoes.Tipo_R: 
+                PCEscCond = "0";
+                PCEsc = "1";
+                louD = "0";
+                LerMem = "1";
+                EscMem = "0";
+                MemParaReg = "0"; //Em 0
+                IREsc = "1";
+                FontePC = "00";
+                ULAOp = "000";
+                ULAFonteB = "01";
+                ULAFonteA = "0";
+                EscReg = "1"; //Em 1
+                RegDst = "1"; //Em 1
+                proximoEstado = estados.BUSCA;
+                return true;
+                break;
+            case instrucoes.Sw:
+                PCEscCond = "0";
+                PCEsc = "1";
+                louD = "1"; //Em 1
+                LerMem = "1";
+                EscMem = "1"; //Em 1
+                MemParaReg = "0";
+                IREsc = "1";
+                FontePC = "00";
+                ULAOp = "000";
+                ULAFonteB = "01";
+                ULAFonteA = "0";
+                EscReg = "0";
+                RegDst = "0";
+                proximoEstado = estados.BUSCA;
+                return true;
+                break;
+            case instrucoes.Lw:
+                PCEscCond = "0";
+                PCEsc = "1";
+                louD = "1";  //Em 1
+                LerMem = "1"; //Em 1
+                EscMem = "0";
+                MemParaReg = "0";
+                IREsc = "1";
+                FontePC = "00";
+                ULAOp = "000";
+                ULAFonteB = "01";
+                ULAFonteA = "0";
+                EscReg = "0";
+                RegDst = "0";
+                proximoEstado = estados.WRITE;
+                return true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public boolean entraWriteBack(){
+        PCEscCond = "0";
+        PCEsc = "1";
+        louD = "0";
+        LerMem = "1";
+        EscMem = "0";
+        MemParaReg = "1"; //Em 1
+        IREsc = "1";
+        FontePC = "00";
+        ULAOp = "000";
+        ULAFonteB = "01";
+        ULAFonteA = "0";
+        EscReg = "1"; //Em 1
+        RegDst = "0"; //Em 0
+        return true;
     }
 
     public boolean avancaEstado(String instrucao){
         estadoAtual = proximoEstado;
         switch (estadoAtual) {
             case estados.BUSCA:
-                buscaInstrucao(instrucao);
                 proximoEstado = estados.DECODE;
-                return false;
+                return buscaInstrucao(instrucao);
             case estados.DECODE:
-                entraDecode();
                 proximoEstado = estados.EXEC;
-                return false;
+                return entraDecode();
             case estados.EXEC:
+                //Decide prox estado no metodo
                 return entraExec();
+            case estados.MEMORIA:
+                //Decide prox estado no metodo
+                return entraMemoria();
+            case estados.WRITE:
+                proximoEstado = estados.BUSCA;
+                return entraWriteBack();
             default:
                 break;
         }
